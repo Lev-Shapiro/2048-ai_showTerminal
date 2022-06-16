57fdc5f57ad8ef48a053b3b61995f2a7979f0605
Lev-Shapiro <levshapiro@Levs-Macbook-Air.local>
2022-06-16 15:22:39 +0300

    v1.0.1
    uncommented factor2

--- src/domain/models/ai.model.ts
+++ src/domain/models/ai.model.ts
@@ -1,4 +1,4 @@
-// import loopDirections from "../../scripts/loop-directions.script";
+import loopDirections from "../../scripts/loop-directions.script";
 import { Direction } from "../entities/direction.entity";
 import { BoardInformation } from "./../entities/board.entity";
 import BoardModel from "./board.model";
@@ -6,15 +6,15 @@ import BoardModel from "./board.model";
 export default class AiModel extends BoardInformation {
     // private depth: number = 0; //* current depth of search tree
 
-    private placeFactor(board: BoardModel, i: number, j: number): number {
-        
-        const boardWeight = this.getBoardWeight();
+    // private placeFactor(board: BoardModel, i: number, j: number): number {
 
-        const placeWeight = boardWeight[i][j];
-        const placeValue = board.board[i][j];
+    //     const boardWeight = this.getBoardWeight();
 
-        return placeValue * placeWeight;
-    }
+    //     const placeWeight = boardWeight[i][j];
+    //     const placeValue = board.board[i][j];
+
+    //     return placeValue * placeWeight;
+    // }
 
     private getBoardReward(board: BoardModel) {
         let reward = 0;
@@ -28,22 +28,24 @@ export default class AiModel extends BoardInformation {
         //* first factor
         reward += board.findEmptyPlaces().length * 0.025;
 
-        // //* second factor
-        // const factor2 = Math.max(...loopDirections((d) => board.findMergesCount(Direction[d]))) * 0.0375;
+        //* second factor
+        const factor2 =
+            Math.max(...loopDirections((_, d) => board.findMergesCount(d))) *
+            0.0375;
 
-        // reward += factor2;
+        reward += factor2;
 
-        //* third factor
-        for (let i = 0; i < this.BOARD_SIZE; i++) {
-            for (let j = 0; j < this.BOARD_SIZE; j++) {
-                const currPlaceFactor = this.placeFactor(board, i, j),
-                    prevPlaceFactor = this.placeFactor(board, i, j - 1);
+        // //* third factor
+        // for (let i = 0; i < this.BOARD_SIZE; i++) {
+        //     for (let j = 0; j < this.BOARD_SIZE; j++) {
+        //         const currPlaceFactor = this.placeFactor(board, i, j),
+        //             prevPlaceFactor = this.placeFactor(board, i, j - 1);
 
-                if (j != 0 && currPlaceFactor > prevPlaceFactor) {
-                    reward += 1 / Math.pow(this.BOARD_SIZE, 2);
-                }
-            }
-        }
+        //         if (j != 0 && currPlaceFactor > prevPlaceFactor) {
+        //             reward += 1 / Math.pow(this.BOARD_SIZE, 2);
+        //         }
+        //     }
+        // }
 
         return reward;
     }
@@ -59,11 +61,11 @@ export default class AiModel extends BoardInformation {
             reward: -1,
         };
 
-        for (var x = 0; x < 60; x++) {
+        for (var x = 0; x < 5; x++) {
             const directions = board.findAvailableDirections();
 
             directions.forEach((direction) => {
-                for (let i = 0; i < 10; i++) {
+                for (let i = 0; i < 2; i++) {
                     var boardCopy = board.copy();
 
                     boardCopy.agentAction(direction);
